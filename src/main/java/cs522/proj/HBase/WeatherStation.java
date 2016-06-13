a7b2818fcfae65d7cab14ccf047317a9f3025504
cloudera <cloudera@quickstart.cloudera>
2016-06-13 05:52:11 -0500

    Modify WeatherStation

--- src/main/java/cs522/proj/HBase/WeatherStation.java
+++ src/main/java/cs522/proj/HBase/WeatherStation.java
@@ -12,28 +12,23 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueLineRecordReader;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
-import cs522.proj.MR.Stripes.ReduceClass;
-
-
 public class WeatherStation extends Configured implements Tool {
 	private static final Logger logger = Logger.getLogger(WeatherStation.class);
+	private static final String columnFamily = "info";
+	private static final String[] qualifiers = {"lat", "lng", "elev", "state", "name"};
 
 	public WeatherStation() {
 		super();
@@ -43,30 +38,34 @@ public class WeatherStation extends Configured implements Tool {
 		super(conf);
 	}
 
-	public static class MapClass extends Mapper<Text, Text, ImmutableBytesWritable, Put> {
-		private MapWritable H;
+	public static class MapClass extends
+			Mapper<LongWritable, Text, ImmutableBytesWritable, Put> {
+		private long checkpoint = 100;
+		private long count = 0;
+		private Put put;
+		private String[] cellStrs = new String[5];
 
 		@Override
-		public void setup(Context context) throws IOException, InterruptedException {
-			H = new MapWritable();
-		}
-
-		private void addMapIntValue(MapWritable map, Writable key, IntWritable newValue) {
-			if (map.containsKey(key)) {
-				IntWritable oldValue = (IntWritable) map.get(key);
-				map.put(key, new IntWritable(oldValue.get() + newValue.get()));
-			} else
-				map.put(key, newValue);
-		}
-
-		@Override
-		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
-
-		}
-
-		@Override
-		public void cleanup(Context context) throws IOException, InterruptedException {
-
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			
+			String valueStr = value.toString();
+			String rowKey = valueStr.substring(0, 11).trim();
+			cellStrs[0] = valueStr.substring(12, 20).trim();
+			cellStrs[1] = valueStr.substring(21, 30).trim();
+			cellStrs[2] = valueStr.substring(31, 37).trim();
+			cellStrs[3] = valueStr.substring(38, 40).trim();
+			cellStrs[4] = valueStr.substring(41, 71).trim();
+			put = new Put(Bytes.toBytes(rowKey));
+			for (int i=0; i<5; i++){
+				if (! cellStrs[i].isEmpty())
+					put.addColumn(Bytes.toBytes(columnFamily),
+							Bytes.toBytes(qualifiers[i]), Bytes.toBytes(cellStrs[i]));
+			}
+			context.write(new ImmutableBytesWritable(Bytes.toBytes(rowKey)), put);
+						
+			if(++count % checkpoint == 0)
+				context.setStatus("Emitting Put " + count);
 		}
 	}
 
@@ -75,56 +74,37 @@ public class WeatherStation extends Configured implements Tool {
 			System.err.println("Usage: weatherstation <input_dir> <table_name>");
 			return 2;
 		}
-		
-		final String columnFamily = "s";
-		final String[] qualifiers = {"Lat", "Lng", "ASL", "Name"};
-		
-		
+
 		TableName tableName = TableName.valueOf(args[1]);
 		Configuration conf = getConf();
-		
-		try (Connection connection = ConnectionFactory.createConnection(conf)){
-			Admin admin = connection.getAdmin();
-		    if(admin.tableExists(tableName)){
-		        logger.info("Table " + args[1] + " exists! Recreating...");
-		        admin.disableTable(tableName);
-		        admin.deleteTable(tableName);
-		    }
-		    
-		    HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
-		    hTableDescriptor.addFamily(new HColumnDescriptor(columnFamily));
-		    admin.createTable(hTableDescriptor);
-		    
-		}
-		
-	    
-	    
 
-		conf.set(KeyValueLineRecordReader.KEY_VALUE_SEPERATOR, " ");
+		try (Connection connection = ConnectionFactory.createConnection(conf)) {
+			Admin admin = connection.getAdmin();
+			if (admin.tableExists(tableName)) {
+				logger.info("Table " + args[1] + " exists! Recreating...");
+				admin.disableTable(tableName);
+				admin.deleteTable(tableName);
+			}
+
+			HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+			hTableDescriptor.addFamily(new HColumnDescriptor(columnFamily));
+			admin.createTable(hTableDescriptor);
+		}
+
 		Job job = Job.getInstance(conf, "WeatherStation");
 		job.setJarByClass(WeatherStation.class);
-		job.setInputFormatClass(KeyValueTextInputFormat.class);
 
 		job.setMapperClass(MapClass.class);
+		TableMapReduceUtil.initTableReducerJob(tableName.getNameAsString(),	null, job);
+		job.setNumReduceTasks(0);
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
-
-	    TableMapReduceUtil.initTableReducerJob(tableName.getNameAsString(), null, job);
-	    job.setNumReduceTasks(0);
-
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 
 	public static void main(String[] args) throws Exception {
-		int ret = ToolRunner.run(new WeatherStation(HBaseConfiguration.create()), args);
+		int ret = ToolRunner.run(
+				new WeatherStation(HBaseConfiguration.create()), args);
 		System.exit(ret);
 	}
 }
-
-/**
- * Sample data:
- * CH000054511  39.9330  116.2830   55.0    BEIJING                        GSN     54511
- * SF001760150 -30.7500   27.0200 1497.0    PAARDEFONTEIN                               
- * USC00132789  41.0211  -91.9553  225.6 IA FAIRFIELD                          HCN      
- * US10red_020  40.2973 -100.2131  737.9 NE CAMBRIDGE 2.7 WNW                           
- */
